9820d2640d12f94c8dead508a47c5b9e66ca16fb
Fauzi Galih Aji Saputro <64176403+fauzigalih@users.noreply.github.com>
2020-10-04 23:44:29 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,4 +7,14 @@ for (var i = 0; i < btns.length; i++) {
     });
 }
 
-if(document.querySelector('main.main')) document.body.style.backgroundColor = 'white';
+if(document.querySelector('main.main') || document.querySelector('main.static')) document.body.style.backgroundColor = 'white';
+
+// if(document.querySelector('main').offsetHeight <= vh(100)) document.querySelector('footer').classList.add('fixed');
+
+// if(window.innerHeight <= '100vh') document.querySelector('footer').classList.add('fixed');
+
+const vh = window.innerHeight;
+const vh1 = $(window).innerHeight;
+const vh2 = $(window).height();
+const vh3 = document.body.offsetHeight;
+console.log(Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0));
